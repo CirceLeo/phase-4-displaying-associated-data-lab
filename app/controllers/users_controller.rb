be27ff1f60cdf965c3d9db93980f40e65d6d1563
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-27 15:52:50 -0400

    commit tree 0fb32550e733e61ced58ae42cf45e60b31d5b0eb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show]
+    def show
+        # user = User.find(params[:id])
+        render json: @user, include: :items
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
 end
